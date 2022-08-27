0e54c5a4e14cb5108846454a7c11f2cd6cb0ae5c
Abdulaziz Alanazi <dev.abdulaziz@outlook.com>
2022-08-27 19:44:20 +0300

    Employees done

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
-
+import {FaEdit} from 'react-icons/fa'
+import {RiDeleteBin6Line} from 'react-icons/ri';
 export default function Employees() {
   return (
-    <div>
-        <h1>
-        Employees
-        </h1>
+    <div className='employees '>
+      <div style={{maxWidth:"90%"}} className='container '>
+        <table className="table ">
+  <thead >
+    <tr>
+      <th>#</th>
+      <th>الإسم</th>
+      <th>الصلاحيات</th>
+      <th>تعديل</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th >1</th>
+      <td>عبدالعزيز</td>
+      <td>مدير كبير</td>
+      <td className='d-flex  justify-content-center'>
+        <button className='btn btn-sm noBorder   text-dark iconCircle   '><FaEdit size={14}/></button> 
+        <button className='btn btn-sm noBorder   text-dark iconCircle'><RiDeleteBin6Line size={15}/></button>
+      </td>
+    </tr>
+    <tr>
+      <th >2</th>
+      <td>ملفي</td>
+      <td>ابو المدير</td>
+      <td className='d-flex  justify-content-center'>
+        <button className='btn btn-sm noBorder   text-dark iconCircle   '><FaEdit size={14}/></button> 
+        <button className='btn btn-sm noBorder   text-dark iconCircle'><RiDeleteBin6Line size={15}/></button>
+      </td>
+    </tr>
+    <tr >
+      <th >3</th>
+      <td>عمر</td>
+      <td>اخوه</td>
+      <td className='d-flex  justify-content-center'>
+        <button className='btn btn-sm noBorder   text-dark iconCircle   '><FaEdit size={14}/></button> 
+        <button className='btn btn-sm noBorder   text-dark iconCircle'><RiDeleteBin6Line size={15}/></button>
+      </td>
+    </tr>
+  </tbody>
+</table>
+    </div>
     </div>
   )
 }
